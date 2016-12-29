b1a3afa677316b9b3ad99d644f1e4461358fd8f6
祝文博 <zhuwenbo@zhujia360.com>
2016-12-29 15:46:47 +0800

    Initial Commit

---
+++ KLMainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KLMainProject
+//
+//  Created by Kevin on 2016/12/29.
+//  Copyright © 2016年 KevinLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
