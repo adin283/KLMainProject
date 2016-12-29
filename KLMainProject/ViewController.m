df96117032fa50a0f00c6f606284e43b8e081c39
祝文博 <zhuwenbo@zhujia360.com>
2016-12-29 20:14:56 +0800

    modular develop main project

--- KLMainProject/ViewController.m
+++ KLMainProject/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "KLProtocolManager.h"
+#import "KLGoodsDetailServiceProtocol.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) UIButton *goodsButton;
 
 @end
 
@@ -17,12 +21,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.navigationItem.title = @"首页";
+    
+    [self.view addSubview:self.goodsButton];
+}
+
+- (void)viewWillLayoutSubviews
+{
+    [super viewWillLayoutSubviews];
+    self.goodsButton.frame = CGRectMake(0, 0, 100, 100);
+    self.goodsButton.center = self.view.center;
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - event
+- (void)didClickGoodsButton:(UIButton *)button
+{
+    id<KLGoodsDetailServiceProtocol> goodsServiceProvider = [KLProtocolManager serviceProviderForProtocol:@protocol(KLGoodsDetailServiceProtocol)];
+    UIViewController *viewController = [goodsServiceProvider goodsDetailViewControllerWithGoodsId:@"123" goodsName:@"房产"];
+    [self.navigationController pushViewController:viewController animated:YES];
+}
+
+#pragma mark - getters
+- (UIButton *)goodsButton
+{
+    if (_goodsButton == nil) {
+        _goodsButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_goodsButton setTitle:@"A商品" forState:UIControlStateNormal];
+        [_goodsButton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [_goodsButton addTarget:self action:@selector(didClickGoodsButton:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _goodsButton;
 }
 
 
---
+++ Podfile
@@ -0,0 +1,28 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+source 'https://github.com/adin283/KRepos.git'
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'KLMainProject' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  use_frameworks!
+  pod "KLProtocolManager"
+  pod "KLGoodsDetailServiceProtocol"
+  pod "KLGoodsDetail"
+  pod "KLConfirmOrderServiceProtocol"
+  pod "KLConfirmOrder"
+
+  # Pods for KLMainProject
+
+  target 'KLMainProjectTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'KLMainProjectUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
